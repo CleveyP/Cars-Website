77a0796748186f0d1eebbf9ded8293d4a10fd501
CleveyP <clevelandplonsey@gmail.com>
2022-05-18 16:14:25 -0700

    add content to footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+<style>
+footer{
+    display: flex;
+    flex-direction:row;
+    justify-content: space-around;
+    align-items:center;   
+    background-color: #242424;
+    height: 60px;
+    width: 100%;
+}
+a{
+    color: #5994A4;
+    text-decoration: none; 
+}
+</style>
+
+<footer>
+    <a href="">Terms</a>
+    <a href="">About Us</a>
+    <a href="">Contact</a>
+</footer>
