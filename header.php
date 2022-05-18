630e392e7a0bc8b55759e1947a992d6e50ff7135
CleveyP <clevelandplonsey@gmail.com>
2022-05-17 17:48:25 -0700

    create header.php index.php styles.css files

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <h1>Best Automotive</h1>
+    <img id="logo" src="" alt="">
+    <form action="search-results.php" method="post">
+        <input id="search-bar" type="text" name="search-bar">
+        <button type="submit" id="search-button">Go!</button>
+    </form>
+    <a href="sell.php"><button id="sell-button">I want to Sell</button></a>
+    <a href="cart.php"><button id="cart"></button></a>
+    <a href="profile.php"><button id="profile">Profile</button></a>
+    <button onclick="logout()">Logout</button>
+</body>
+
+<script>
+    let logout = () =>{
+        //set the session variables to undefined
+    }
+</script>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Automotive</title>
+</head>
+<body>
+    <?php include("header.php"); ?>
+    <main>
+        <div id="hero-container">
+            <img id="hero" src="" alt="">
+            
+        </div>
+
+
+
+    </main>
+    
+</body>
+</html>
