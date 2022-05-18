9845029d837414ee5a8d5afb507c8198ede69a13
CleveyP <clevelandplonsey@gmail.com>
2022-05-17 17:54:27 -0700

    update styles.css

--- header.php
+++ header.php
@@ -2,16 +2,18 @@
 <html lang="en">
 
 <body>
-    <h1>Best Automotive</h1>
-    <img id="logo" src="" alt="">
-    <form action="search-results.php" method="post">
-        <input id="search-bar" type="text" name="search-bar">
-        <button type="submit" id="search-button">Go!</button>
-    </form>
-    <a href="sell.php"><button id="sell-button">I want to Sell</button></a>
-    <a href="cart.php"><button id="cart"></button></a>
-    <a href="profile.php"><button id="profile">Profile</button></a>
-    <button onclick="logout()">Logout</button>
+    <header>
+        <h1>Best Automotive</h1>
+        <img id="logo" src="" alt="">
+        <form action="search-results.php" method="post">
+            <input id="search-bar" type="text" name="search-bar">
+            <button type="submit" id="search-button">Go!</button>
+        </form>
+        <a href="sell.php"><button id="sell-button">I want to Sell</button></a>
+        <a href="cart.php"><button id="cart"></button></a>
+        <a href="profile.php"><button id="profile">Profile</button></a>
+        <button onclick="logout()">Logout</button>
+    </header>
 </body>
 
 <script>
