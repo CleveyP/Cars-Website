a1cf33926bb1786d4b4c79830e86fb3abac49b07
Evan Giampaoli <Evan Giampaoli>
2022-06-23 16:39:00 -0700

    styled header

--- php/header.php
+++ php/header.php
@@ -9,7 +9,7 @@
         <img id="logo" src="" alt="">
         <form action="search-results.php" method="post">
             <input id="search-bar" type="text" name="search-bar" placeholder="Search by Make or Model">
-            <button type="submit" id="search-button">Go!</button>
+            <!-- <button type="submit" id="search-button">Go!</button> -->
         </form>
         <a href="sell.php"><button id="sell-button">I want to Sell</button></a>
         <a href="cart.php"><button id="cart">Checkout</button></a>
