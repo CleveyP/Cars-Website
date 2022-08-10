192f7d40bb739a666498475bb612f489229e5edc
Evan Giampaoli <Evan Giampaoli>
2022-08-09 19:15:29 -0700

    session destroy logout.php

--- php/get_user_info.php
+++ php/get_user_info.php
@@ -12,13 +12,19 @@ $password = $_POST['password'];
 $user_email = $_POST['email'];
 // retrieve user data from backend
 $user_email = "'" . $user_email . "'";
+echo $password . " ---- " . $user_email . "<br>";
 $sql = "SELECT * from users WHERE email = $user_email";
 $result = mysqli_query($conn, $sql);
+echo $sql . "<br>";
 
 if ($result) {
     $user_info = mysqli_fetch_assoc($result);
     // set session variables
 
+    echo strlen($user_info['h_password']) . "<br>";
+    echo strlen($password) . "<br>";
+    $test = password_verify($password, $user_info["h_password"]) ? "true" : "false";
+    echo strval($test) . "<br>";
     if (password_verify($password, $user_info["h_password"])) {
         $_SESSION["isLoggedIn"] = TRUE;
         $_SESSION["firstname"] = $user_info['firstname'];
