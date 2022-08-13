4ea2e3f18828021b9fa10860287c787c772c19f6
CleveyP <clevelandplonsey@gmail.com>
2022-08-12 17:25:24 -0700

    fix process_sell.php

--- php/process_sell.php
+++ php/process_sell.php
@@ -1,5 +1,5 @@
 <?php
-
+include('header.php');
 
 //upload the file
 $file = $_FILES['car-photo'];
@@ -16,7 +16,7 @@ if(in_array($file_ext, $allowed_extensions)){
         $file_name_new = uniqid('', true).".".$file_ext; //rename the file 
         $file_dest = '../pictures/'.$file_name_new; //set the destination folder for the file
         move_uploaded_file($file_Tmpname, $file_dest); //move the file into the destination folder.
-        echo "successfully uploaded file!";
+        
     }
     else{
         echo "There was an error uploading the file.";
@@ -39,18 +39,22 @@ $product_year = $_POST['year'];
 $product_name = $product_year . " " . $product_make . " " . $product_model;
 $product_mileage = $_POST['mileage'];
 $product_stock = 1;
-$product_is_new = ($product_mileage === 0) ? 'TRUE' : 'FALSE';
+$product_is_new = ($product_mileage === '0') ? 'TRUE' : 'FALSE';
 $sql = "INSERT INTO products (product_name, product_make, product_model, product_price, product_mileage, product_stock, product_is_new, product_year, product_color, product_image_path) 
 VALUES    ('" . $product_name ."', '" . $product_make ."', '" . $product_model ."', ". $product_price .", ".$product_mileage.", 1, " . $product_is_new . ", ". $product_year  .", '". $product_color . "', '" . $file_name_new . "')";
-echo $sql;
+
 
 //launch the insert query
 $result = mysqli_query($conn, $sql);
 if($result){
-    echo "successfully inserted new row to product table!";
+    echo "<h1 id='successful-upload-message'> Success! Your car is now up for sale!</h1>
+    <img src='../pictures/car_and_money.jpg' alt='cash and car' id='successful-upload-img' />
+    ";
+   
 }
 else{
     echo "did NOT successfully add row...";
 }
+include('footer.php');
 ?>
 
