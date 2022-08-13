cbbc2cd57996d7d11d9e648ab132514462dd4740
CleveyP <clevelandplonsey@gmail.com>
2022-08-12 17:25:05 -0700

    create change_username.php

---
+++ php/change_username.php
@@ -0,0 +1,30 @@
+
+    <?php
+    session_start();
+
+        include("db_settings.php");
+        $conn = mysqli_connect($server, $user, $password, $database);
+        if ($conn) {
+                $userid =  "'" . $_SESSION['user_id'] . "'";
+                
+                $new_firstname = $_POST['new-firstname'];
+                $new_lastname = $_POST['new-lastname'];
+                $sql = "UPDATE users SET firstname = $new_firstname, lastname = $new_lastname WHERE id = $userid";
+                $result = mysqli_query($conn, $sql);
+                if (!$result) {
+                    echo $sql . mysqli_error($conn);
+                }
+                else{
+                    ob_start();
+                    header("Location: profile.php");
+                    ob_end_flush();
+                    die();
+                }
+            }
+            else {
+                ob_start();
+                header("Location: change_username_form.php");
+                ob_end_flush();
+                die();
+            }
+    ?>
