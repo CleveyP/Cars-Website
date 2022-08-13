26b63bc9c6a06ede0bda5c8244600fd8ae973523
CleveyP <clevelandplonsey@gmail.com>
2022-08-12 17:25:45 -0700

    fix profile.php

--- php/profile.php
+++ php/profile.php
@@ -9,7 +9,7 @@
     echo "<h2 id='username'>" . $firstname . " " . $lastname . "'s Profile<br>
     <section id='edit-profile'>
         <a href='change_password_form.php' id='change-password-a'>Change password</a><br>
-        <a href='change_username.php' id='change-username-a'>Change Username</a><br>
+        <a href='change_username_form.php' id='change-username-a'>Change Username</a><br>
         <form id='dark-form' method='post' action='toggle_dark.php'>
             <button id='dark-mode-button' name='dark-mode-button' value='" . $darkValue . "' type='submit'>Toggle Dark Mode</button>
         </form>
