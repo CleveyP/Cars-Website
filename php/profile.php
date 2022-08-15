71f6ffe1ea3281e0228a703db9968ccad09a1d2a
CleveyP <clevelandplonsey@gmail.com>
2022-08-15 15:54:18 -0700

    style profile,php

--- php/profile.php
+++ php/profile.php
@@ -6,13 +6,11 @@
     $lastname = trim($_SESSION['lastname']);
 
     $darkValue = $_SESSION['dark_value'];
-    echo "<h2 id='username'>" . substr($firstname, 1, -1) . " " . $lastname . "'s Profile<br>
+    echo "<h2 id='username'>" . substr($firstname, 1, -1) . " " . $lastname . "'s Profile</h2><br>
     <section id='edit-profile'>
-        <a href='change_password_form.php' id='change-password-a'>Change password</a><br>
-        <a href='change_username_form.php' id='change-username-a'>Change Username</a><br>
-        <form id='dark-form' method='post' action='toggle_dark.php'>
-            <button id='dark-mode-button' name='dark-mode-button' value='" . $darkValue . "' type='submit'>Toggle Dark Mode</button>
-        </form>
+        <h3 id='change-user-credentials'>Danger field!</h3>
+        <a href='change_password_form.php' id='change-password-a' class='change-user-a'>Change password</a><br>
+        <a href='change_username_form.php' id='change-username-a' class='change-user-a'>Change Username</a><br>
     </section>";
 
     include('footer.php');
